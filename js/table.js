63fbed5187ed96f5b2a1b8d51afcd56eabe8e2bc
Joe Cserko <j.cserko@gmail.com>
2022-06-01 11:30:23 +0200

    done

---
+++ js/table.js
@@ -0,0 +1,50 @@
+// Táblázat oszlopai
+const columns = [
+    {title: 'Name', key: 'name'},
+    {title: 'Monthly', key: 'price.monthly'},
+    {title: 'Annual', key: 'price.annual'},
+    {title: 'Disk space', key: 'services[0].value'},
+    {title: 'Email accounts', key: 'services[1].value'},
+    {title: 'Bandwith', key: 'services[2].value'},
+    {title: 'Subdomains', key: 'services[3].value'},
+    {title: 'Domains', key: 'services[4].value'},
+];
+
+// Táblázat rajzolása
+const generatePricingTable = (plans) => {
+    const container = document.querySelector('.container');
+    const table = document.createElement('table');
+    table.className = 'table table-striped';
+    container.appendChild(table);
+
+    const thead = document.createElement('thead');
+    table.appendChild(thead);
+    const theadRow = document.createElement('tr');
+    thead.appendChild(theadRow);
+
+    columns.forEach( col => {
+        const th = document.createElement('th');
+        theadRow.appendChild(th);
+        th.textContent = col.title;
+    });
+
+    const tbody = document.createElement('tbody');
+    table.appendChild(tbody);
+
+    plans.forEach( plan => {
+        const tr = document.createElement('tr');
+        tbody.appendChild(tr);
+
+        columns.forEach( col => {
+            const td = document.createElement('td');
+            tr.appendChild(td);
+            td.textContent = _.get(plan, col.key);
+        });
+    });
+};
+
+
+// Fetch - adatok letöltése
+fetch('https://raw.githubusercontent.com/Training360/JAVASCRIPT-alapok-dom-es6/main/json/plans.json')
+    .then( response => response.json() )
+    .then( plans => generatePricingTable(plans) );
